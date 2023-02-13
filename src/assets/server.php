7fd0dcfe44f7c08bc23e5b53dd27a52a09a9901e
Oleksandr Ubohyi <global061@gmail.com>
2023-02-12 21:23:49 -0600

    added forms to submit

--- src/assets/server.php
+++ src/assets/server.php
@@ -1,2 +1,3 @@
+//returns a string of user form data
 <?php
 echo var_dump($_POST);
---
+++ src/js/modules/forms.js
@@ -0,0 +1,74 @@
+const forms = () => {
+    const form = document.querySelectorAll('form'),
+          input = document.querySelectorAll('input'),
+          phoneInputs = document.querySelectorAll('input[name="user_phone"]');
+
+    phoneInputs.forEach(item => {
+        item.addEventListener('input', () => {
+            //regexp to enter only numbers
+            item.value = item.value.replace(/\D/, '');
+        })
+    });
+
+
+    const message = {
+        loading: 'Загрузка...',
+        success: 'Спасибо! Скоро мы с вами свяжемся',
+        failure: 'Что-то пошло не так...'
+    };
+
+    //async - tell JS there are async operations so JS knows about it, and then put Await before async operations
+    const postData = async (url, data) => {
+        document.querySelector('.status').textContent = message.loading;
+        //fetch API returns promise
+        //add headers to our feth request
+        // ES7 - async/await
+        //we need to wait for the result to save it, so we need to Await
+        let res = await fetch(url, {
+            method: "POST",
+            body: data
+        });
+
+        //we know server return async text data, so we have to await here as well
+        return await res.text();
+    }
+
+    const clearInputs = () => {
+        input.forEach(item => {
+            item.value = '';
+        });
+    }
+
+    form.forEach(item => {
+        item.addEventListener('submit', e => {
+            e.preventDefault();
+
+            //create status message container
+            let statusMessage = document.createElement('div');
+            //add styles to it
+            statusMessage.classList.add('status');
+            //add container to the form submitted
+            item.appendChild(statusMessage);
+
+            //collect all form data including pictures, files etc.
+            //we need to know what server takes: urlencoded format, json, formdata
+            const formData = new FormData(item);
+
+            postData('assets/server.php', formData)
+            .then(res => {
+                console.log(res);
+                statusMessage.textContent = message.success;
+            })
+            .catch(()=> statusMessage.textContent = message.failure)
+            .finally(()=> {
+                clearInputs();
+                setTimeout(() => {
+                   statusMessage.remove() 
+                }, 5000);
+            })
+
+        })
+    });
+}
+
+export default forms;
